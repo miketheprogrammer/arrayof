002c05dc8f7fde1f258af992173d302d453e200f
Michael Hernandez <mhernandez@jibe.com>
2014-02-19 14:56:53 -0500

    Trying to fix for ie < 9

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 Array.of = module.exports = Array.of || function () {
   var array = new Array(arguments.length)
-  for (var key in arguments) {
-    array[parseInt(key)] = arguments[key];
+  for (n = 0; n < arguments.length; n++){
+    array[n] = arguments[n];
   }
   return array;
 };
