00b242e9e69e2870fa1790ed4d6afb7685b0247d
Michael Hernandez <mhernandez@jibe.com>
2014-02-19 14:58:30 -0500

    changed to += op instead of ++

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 Array.of = module.exports = Array.of || function () {
   var array = new Array(arguments.length)
-  for (n = 0; n < arguments.length; n++){
+  for (n = 0; n < arguments.length; n += 1){
     array[n] = arguments[n];
   }
   return array;
