e3d29c7d3bb8da68476e12f70b37c65655a60516
Michael Hernandez <mhernandez@jibe.com>
2014-02-19 14:59:19 -0500

    normalize

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 Array.of = module.exports = Array.of || function () {
   var array = new Array(arguments.length)
-  for (n = 0; n < arguments.length; n += 1){
-    array[n] = arguments[n];
+  for (var i = 0; i < arguments.length; i += 1){
+    array[i] = arguments[i];
   }
   return array;
 };
